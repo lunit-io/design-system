0a39b5066ea7e3cff02fe49d7f6d02bf588aa6c4
SangYeob Yu <syyu@lunit.io>
2022-01-18 16:12:07 +0900

    docs: setup storybook for docs

---
+++ docs/.storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  staticDirs: ["../public"],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+};
