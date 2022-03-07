1089bbf6f19313526ff282f57909554882eba010
SangYeob Yu <syyu@lunit.io>
2022-03-08 00:38:16 +0900

    chore(icons): webpack build config

--- packages/design-system-icons/webpack.config.js
+++ packages/design-system-icons/webpack.config.js
@@ -1,7 +1,14 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
 module.exports = {
+  mode: "production",
   entry: "./src/index.ts",
   output: {
-    filename: "./dist/bundle.js",
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "umd",
+    clean: true,
   },
   // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
@@ -12,9 +19,20 @@ module.exports = {
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+        options: {
+          configFile: "tsconfig.build.json",
+        },
+      },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
       {
         test: /\.svg$/,
         issuer: /\.[jt]sx?$/,
@@ -22,5 +40,6 @@ module.exports = {
       },
     ],
   },
-  // Other options...
+  externalsPresets: { node: true },
+  externals: [nodeExternals()],
 };
