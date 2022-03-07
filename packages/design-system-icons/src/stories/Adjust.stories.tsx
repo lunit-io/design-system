f07b8892604ae8409f521974ffdfb64c1fc47a7f
SangYeob Yu <syyu@lunit.io>
2022-03-07 16:48:42 +0900

    feat(icons): setup storybook for icons

---
+++ packages/design-system-icons/src/stories/Adjust.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta } from "@storybook/react";
+import { AdjustIcon } from "../index";
+
+export default {
+  title: "Common/Adjust",
+  component: AdjustIcon,
+} as ComponentMeta<typeof AdjustIcon>;
+
+export const Adjust = () => <AdjustIcon />;
---
+++ packages/design-system-icons/src/stories/Analyze.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta } from "@storybook/react";
+import { AnalyzeIcon } from "../index";
+
+export default {
+  title: "Common/Analyze",
+  component: AnalyzeIcon,
+} as ComponentMeta<typeof AnalyzeIcon>;
+
+export const Analyze = () => <AnalyzeIcon />;
