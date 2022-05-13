3cc9615cf893de1a9e833c3125869b895dd6ec26
Hyunjin Kwon <hyunjin.kwon@lunit.io>
2022-05-13 21:06:04 +0900

    chore(design-system): customize storybook theming

---
+++ packages/design-system/.storybook/Welcome/Welcome.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Welcome as Intro } from "./Welcome";
+
+export default {
+  title: "Getting Started/Welcome",
+  component: Intro,
+};
+
+export const Welcome = () => <Intro />;
