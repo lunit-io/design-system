68802984b5335b8517c8814fee3b11d0991ac6c3
양혜진 <hjyang@lunit.io>
2022-06-07 23:11:02 +0900

    fix(chip): change folder name as chip

--- packages/design-system/src/stories/components/Chip/Chip.stories.tsx
+++ packages/design-system/src/stories/components/Chip/Chip.stories.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import Chip from "@/components/Chips/Chip";
+import Chip from "@/components/Chip/Chip";
 
 export default {
-  title: "Components/Chips",
+  title: "Components/Chip",
   component: Chip,
 } as ComponentMeta<typeof Chip>;
 
