71a3167522b364732d2d6135dfd5dafdbba19a8a
양혜진 <hjyang@lunit.io>
2022-06-07 23:13:36 +0900

    fix(tooltip): change folder name as tooltip

--- packages/design-system/src/stories/components/Tooltip/Tooltip.stories.tsx
+++ packages/design-system/src/stories/components/Tooltip/Tooltip.stories.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import Tooltip from "@/components/Tooltips/Tooltip";
+import Tooltip from "@/components/Tooltip/Tooltip";
 
 export default {
-  title: "Components/Tooltips",
+  title: "Components/Tooltip",
   component: Tooltip,
 } as ComponentMeta<typeof Tooltip>;
 
