db27f74654977658d757a2461db4c41e9afd832a
Hyunjin Kwon <hyunjin.kwon@lunit.io>
2022-06-07 20:53:30 +0900

    feat(design-system): add typography placeholder module and story

---
+++ packages/design-system/src/stories/foundation/Typography/Typography.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+const TypographyComponent = () => {
+  return null;
+};
+
+export default {
+  title: "Foundation/Typography",
+  component: TypographyComponent,
+} as ComponentMeta<typeof TypographyComponent>;
+
+export const Typography: ComponentStory<typeof TypographyComponent> = () => <TypographyComponent />;
