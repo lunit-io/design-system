ec7a31142d607c38704423ee99e1f11312f6380d
Takhyun Kim <kimkih1218@lunit.io>
2022-06-08 00:21:17 +0900

    feat(textfield): add textField component and story

---
+++ packages/design-system/src/stories/components/TextField/TextField.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import TextField from "@/components/TextField/TextField";
+
+export default {
+  title: "Components/TextField",
+  component: TextField,
+} as ComponentMeta<typeof TextField>;
+
+const Template: ComponentStory<typeof TextField> = () => <TextField />;
+
+export const LunitTextField = Template.bind({});
