fba940f7f3cb59c6020253fd8075825f70439d83
양혜진 <hjyang@lunit.io>
2022-11-02 23:01:38 +0900

    chore(chip): add background setting

--- packages/design-system/src/stories/components/Chip/Chip.stories.tsx
+++ packages/design-system/src/stories/components/Chip/Chip.stories.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Box } from "@mui/material";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Chip from "@/components/Chip/Chip";
@@ -10,7 +11,43 @@ export default {
 } as ComponentMeta<typeof Chip>;
 
 const Template: ComponentStory<typeof Chip> = (args: ChipProps) => (
-  <Chip {...args} />
+  <Box sx={{ margin: 10 }}>
+    <Chip {...args} />
+  </Box>
 );
 
 export const LunitChip = Template.bind({});
+LunitChip.parameters = {
+  /**
+   * background colors are set by the rule of Figma's color token
+   */
+  backgrounds: {
+    default: "base00",
+    values: [
+      {
+        name: "base00",
+        value: "#FFFFFF",
+      },
+      {
+        name: "base10",
+        value: "#FFFFFF",
+      },
+      {
+        name: "base70",
+        value: "#4E4E50",
+      },
+      {
+        name: "base80",
+        value: "#333336",
+      },
+      {
+        name: "base85",
+        value: "#2D2D2F",
+      },
+      {
+        name: "base90",
+        value: "#1F1F21",
+      },
+    ],
+  },
+};
