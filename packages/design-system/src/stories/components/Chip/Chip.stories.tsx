b09d55e07a042fe6ce41d34778f5e5a2f3973f3a
양혜진 <hjyang@lunit.io>
2022-11-16 17:23:32 +0900

    feat(chip): add customizable examples

--- packages/design-system/src/stories/components/Chip/Chip.stories.tsx
+++ packages/design-system/src/stories/components/Chip/Chip.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box } from "@mui/material";
+import HeartBrokenOutlinedIcon from "@mui/icons-material/HeartBrokenOutlined";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Chip from "@/components/Chip/Chip";
@@ -16,8 +17,29 @@ const Template: ComponentStory<typeof Chip> = (args: ChipProps) => (
    * TODO
    * Since Color Token system will be changed, below className should be changed accordingly
    */
-  <Box className="base00">
+  <Box className="base00" display="flex" sx={{ gap: "10px" }}>
     <Chip {...args} />
+    <Chip clickable={false} label="outlined chip" sx={{ width: "200px" }} />
+    <Chip
+      clickable
+      label="contained"
+      onClick={() => console.log("clicked")}
+      onDelete={() => console.log("delete")}
+    />
+    <Chip clickable label="contained" />
+    <Chip
+      clickable
+      label="contained"
+      onClick={() => console.log("clicked")}
+      thumbnail="avatar"
+    />
+    <Chip
+      clickable
+      label="contained"
+      onClick={() => console.log("clicked")}
+      onDelete={() => console.log("delete")}
+      thumbnail={<HeartBrokenOutlinedIcon />}
+    />
   </Box>
 );
 
