c589080a825d1d90086b0fc343ba9123d0f4925c
양혜진 <hjyang@lunit.io>
2022-11-22 22:37:00 +0900

    feat(chip): add named exports & description

--- packages/design-system/src/stories/components/Chip/Chip.stories.tsx
+++ packages/design-system/src/stories/components/Chip/Chip.stories.tsx
@@ -1,66 +1,95 @@
 import React from "react";
 import { Box } from "@mui/material";
-import HeartBrokenOutlinedIcon from "@mui/icons-material/HeartBrokenOutlined";
+import { Error } from "@lunit/design-system-icons";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Chip from "@/components/Chip/Chip";
 
-import type { ChipProps } from "@/components/Chip/Chip.types";
-
 export default {
-  title: "Components/Chip",
+  title: "Components/Chips",
   component: Chip,
+  parameters: {
+    docs: {
+      description: {
+        component: `Chips are compact elements that represent an input, attribute, or action.
+        see [Material-UI Chip](https://mui.com/components/chips/)`,
+      },
+    },
+    /**
+     * Todo
+     * below background color is a temporary solution for showing the color code
+     * after the color system is updated, it will be removed
+     */
+    backgrounds: {
+      default: "Dark1",
+      values: [
+        {
+          name: "Light1",
+          value: "#FFFFFF",
+        },
+        {
+          name: "Dark1",
+          value: "#111113",
+        },
+      ],
+    },
+  },
+  decorators: [
+    /**
+     * TODO
+     * Since Color Token system will be changed, below className should be changed accordingly
+     * Also, the hover color will be set again with the color token system completion
+     */
+    (Story) => (
+      <Box className="base90" sx={{ margin: "3em" }}>
+        {Story()}
+      </Box>
+    ),
+  ],
 } as ComponentMeta<typeof Chip>;
 
-const Template: ComponentStory<typeof Chip> = (args: ChipProps) => (
-  /**
-   * TODO
-   * Since Color Token system will be changed, below className should be changed accordingly
-   */
-  <Box className="base00" display="flex" sx={{ gap: "10px" }}>
-    <Chip {...args} />
-    <Chip clickable={false} label="outlined chip" sx={{ width: "200px" }} />
-    <Chip
-      clickable
-      label="contained"
-      onClick={() => console.log("clicked")}
-      onDelete={() => console.log("delete")}
-    />
-    <Chip clickable label="contained" />
-    <Chip
-      clickable
-      label="contained"
-      onClick={() => console.log("clicked")}
-      thumbnail="avatar"
-    />
-    <Chip
-      clickable
-      label="contained"
-      onClick={() => console.log("clicked")}
-      onDelete={() => console.log("delete")}
-      thumbnail={<HeartBrokenOutlinedIcon />}
-    />
-  </Box>
-);
+const Template: ComponentStory<typeof Chip> = (args) => <Chip {...args} />;
 
-export const LunitChip = Template.bind({});
-LunitChip.parameters = {
-  /**
-   * TODO
-   * Below code is just an example before color system is completed
-   * Background colors are set by the rule of Figma's color token
-   */
-  backgrounds: {
-    default: "Light1",
-    values: [
-      {
-        name: "Light1",
-        value: "#FFFFFF",
-      },
-      {
-        name: "Dark1",
-        value: "#111113",
-      },
-    ],
-  },
+export const Outlined = Template.bind({});
+Outlined.args = {
+  label: "Label",
+  color: "primary",
+  clickable: false,
+};
+
+export const Contained = Template.bind({});
+Contained.args = {
+  label: "Label",
+  color: "primary",
+  clickable: true,
+};
+
+export const ContainedWithAvatar = Template.bind({});
+ContainedWithAvatar.args = {
+  ...Contained.args,
+  thumbnail: "avatar",
+};
+
+export const ContainedWithLogo = Template.bind({});
+ContainedWithLogo.args = {
+  ...Contained.args,
+  thumbnail: "logo",
+};
+
+export const ContainedWithIcon = Template.bind({});
+ContainedWithIcon.args = {
+  ...Contained.args,
+  thumbnail: (
+    <Error
+      sx={{
+        paddingLeft: "1.5px",
+      }}
+    />
+  ),
+};
+
+export const ContainedWithDelete = Template.bind({});
+ContainedWithDelete.args = {
+  ...Contained.args,
+  onDelete: () => {},
 };
