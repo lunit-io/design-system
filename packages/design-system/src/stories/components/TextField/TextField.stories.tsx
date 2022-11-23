72854722f1f9e5f6e19d9a8e6a7dabdcf6a08279
Takhyun Kim <kimkih1218@lunit.io>
2022-11-23 19:56:51 +0900

    feat(text-field): wrap the TextField component in Storybook setting

--- packages/design-system/src/stories/components/TextField/TextField.stories.tsx
+++ packages/design-system/src/stories/components/TextField/TextField.stories.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Box } from "@mui/material";
+
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import TextField from "@/components/TextField/TextField";
 
 export default {
   title: "Components/TextField",
   component: TextField,
+  decorators: [
+    (Story) => (
+      <Box className="base00" sx={{ margin: "3em" }}>
+        {Story()}
+      </Box>
+    ),
+  ],
 } as ComponentMeta<typeof TextField>;
 
 const Template: ComponentStory<typeof TextField> = () => <TextField />;
