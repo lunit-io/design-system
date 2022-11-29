07c6727d1429c3dfc5f1e4ac8d955083deee64bf
seunghyunOh <sh5h@lunit.io>
2022-11-29 16:26:14 +0900

    feat(storybook): add plugin for element state

--- packages/design-system/src/components/Button/Button.tsx
+++ packages/design-system/src/components/Button/Button.tsx
@@ -3,7 +3,12 @@ import React from "react";
 import { Button as MuiButton } from "@mui/material";
 
 const Button = () => {
-  return <MuiButton variant="contained">Button</MuiButton>;
+  // 예제 코드이기 떄문에 지워도 상관없습니다.
+  return (<>
+    <MuiButton className="one" variant="contained">Button</MuiButton>
+    <MuiButton className="two" variant="contained">Button</MuiButton>
+    <MuiButton className="three" variant="contained">Button</MuiButton>
+  </>);
 };
 
 export default Button;
