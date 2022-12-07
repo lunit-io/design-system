2ffa8faa9b0cde3d733ed02150391ed60fea89c5
Takhyun Kim <kimkih1218@lunit.io>
2022-12-07 14:35:18 +0900

    feat(text-field): apply Mui material TextField and focused, text color

--- packages/design-system/src/components/TextField/TextField.style.ts
+++ packages/design-system/src/components/TextField/TextField.style.ts
@@ -1,13 +1,29 @@
-import MuiInputUnstyled from "@mui/base/InputUnstyled";
+import MuiTextField from "@mui/material/TextField";
 import { styled } from "@mui/system";
 
-const BaseTextField = styled(MuiInputUnstyled)(({ theme }) => ({
-  "& input": {
+const BaseTextField = styled(MuiTextField)(({ theme }) => ({
+  "& .MuiOutlinedInput-root": {
+    "&.Mui-focused fieldset": {
+      border: `1px solid ${theme.palette.token.core.focused}`,
+    },
+    // TODO: Hover border 색상 확인 필요 디자인과 다른 색상으로 나옴
+    // "&:hover fieldset": {
+    //   backgroundColor: theme.palette.token.core.hover,
+    // },
+
     border: "1px solid #ccc",
     borderRadius: "5px",
     padding: "5px 10px",
     background: theme.palette.token.component.textfield_bg,
     overflow: "hidden",
+    color: theme.palette.token.core.text_normal,
+  },
+
+  "& input, textarea": {
+    padding: 0,
+    "&::placeholder": {
+      color: theme.palette.token.core.text_medium,
+    },
   },
 }));
 
