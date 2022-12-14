59627bac560de45e0a05b823f6e3787605b3c81e
Seonghun Bae <sh.bae@lunit.io>
2022-12-14 16:02:57 +0900

    feat(alert): connect alert component to alert stroybook

--- packages/design-system/src/stories/components/Alert/Alert.stories.tsx
+++ packages/design-system/src/stories/components/Alert/Alert.stories.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Alert from "@/components/Alert";
@@ -6,8 +7,33 @@ import Alert from "@/components/Alert";
 export default {
   title: "Components/Alert",
   component: Alert,
+  argTypes: {
+    severity: {
+      control: "radio",
+      options: ["success", "info", "warning", "error"],
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: `Chips are compact elements that represent an input, attribute, or action.
+        see [Material-UI Chip](https://mui.com/components/chips/)`,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <Box className="base10" sx={{ margin: "3em" }}>
+        {Story()}
+      </Box>
+    ),
+  ],
 } as ComponentMeta<typeof Alert>;
 
-const Template: ComponentStory<typeof Alert> = () => <Alert />;
+const Template: ComponentStory<typeof Alert> = (args) => <Alert {...args} />;
 
 export const LunitAlert = Template.bind({});
+LunitAlert.args = {
+  severity: "success",
+  title: "This is Title",
+};
