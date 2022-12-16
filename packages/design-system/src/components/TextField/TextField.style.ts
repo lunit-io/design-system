acb1ee7c73b042c92a3f6208d39f943ae7916ef2
Takhyun Kim <kimkih1218@lunit.io>
2022-12-16 15:26:33 +0900

    feat(text-field): add error, disabled, helper text style

--- packages/design-system/src/components/TextField/TextField.style.ts
+++ packages/design-system/src/components/TextField/TextField.style.ts
@@ -3,22 +3,31 @@ import { styled } from "@mui/system";
 
 const BaseTextField = styled(MuiTextField)(({ theme }) => ({
   "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      border: "none",
+    },
+    "&.Mui-error fieldset": {
+      border: `1px solid ${theme.palette.token.component.textfield_border_error}`,
+    },
     "&.Mui-focused fieldset": {
       border: `1px solid ${theme.palette.token.core.focused}`,
     },
-    // TODO: Hover border 색상 확인 필요 디자인과 다른 색상으로 나옴
-    // "&:hover fieldset": {
-    //   backgroundColor: theme.palette.token.core.hover,
-    // },
-
-    border: "1px solid #ccc",
-    borderRadius: "5px",
+    "&.Mui-disabled fieldset": {
+      opacity: 0.38,
+    },
     padding: "5px 10px",
     background: theme.palette.token.component.textfield_bg,
     overflow: "hidden",
     color: theme.palette.token.core.text_normal,
   },
-
+  "& .MuiFormHelperText-root": {
+    "&.Mui-error": {
+      color: theme.palette.token.core.text_error,
+    },
+    margin: 0,
+    marginTop: "4px",
+    paddingLeft: "4px",
+  },
   "& input, textarea": {
     padding: 0,
     "&::placeholder": {
