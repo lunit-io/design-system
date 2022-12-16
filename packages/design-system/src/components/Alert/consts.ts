aab1e9a9d4dd7f5db02d036c67ec338586343130
Seonghun Bae <sh.bae@lunit.io>
2022-12-16 15:45:18 +0900

    feat(alert): add color selectors for bg, border, icon color

--- packages/design-system/src/components/Alert/consts.ts
+++ packages/design-system/src/components/Alert/consts.ts
@@ -1,15 +1,20 @@
 import theme from "@/theme";
 import { AlertColor } from "@mui/material";
 
-export const ALERT_COLORS = {
-  INFO: "info",
-  ERROR: "error",
-  WARNING: "warning",
-  SUCCESS: "success",
-} as const;
+export const getBackgroundColor = (severity: AlertColor | undefined) => {
+  switch (severity) {
+    case "info":
+      return theme.palette.token.component.alert_info_bg;
+    case "warning":
+      return theme.palette.token.component.alert_warning_bg;
+    case "error":
+      return theme.palette.token.component.alert_error_bg;
+    default:
+      return theme.palette.token.component.alert_success_bg;
+  }
+};
 
-export const getAccentColor = (severity: AlertColor | undefined) => {
-  console.log("severity :", severity);
+export const getBorderColor = (severity: AlertColor | undefined) => {
   switch (severity) {
     case "info":
       return theme.palette.token.component.alert_info_border;
@@ -19,5 +24,18 @@ export const getAccentColor = (severity: AlertColor | undefined) => {
       return theme.palette.token.component.alert_error_border;
     default:
       return theme.palette.token.component.alert_success_border;
+  }
+};
+
+export const getIconColor = (severity: AlertColor | undefined) => {
+  switch (severity) {
+    case "info":
+      return theme.palette.token.core.icon_info_02;
+    case "warning":
+      return theme.palette.token.core.icon_warning_02;
+    case "error":
+      return theme.palette.token.core.icon_error_02;
+    default:
+      return theme.palette.token.core.icon_sucess_02;
   }
 };
