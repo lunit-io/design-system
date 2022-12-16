d21c9c7a7bf704b353dfafed5ede021380409760
Seonghun Bae <sh.bae@lunit.io>
2022-12-16 15:46:21 +0900

    feat(alert): add light, dark box & add some of args

--- packages/design-system/src/stories/components/Alert/Alert.stories.tsx
+++ packages/design-system/src/stories/components/Alert/Alert.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Alert from "@/components/Alert";
@@ -23,8 +23,31 @@ export default {
   },
   decorators: [
     (Story) => (
-      <Box className="base10" sx={{ margin: "3em" }}>
-        {Story()}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1em",
+        }}
+      >
+        <Box
+          className="base10"
+          bgcolor={"lunit.grey.00.main"}
+          sx={{
+            padding: "3em",
+          }}
+        >
+          {Story()}
+        </Box>
+        <Box
+          className="base90"
+          bgcolor={"lunit.grey.90.main"}
+          sx={{
+            padding: "3em",
+          }}
+        >
+          {Story()}
+        </Box>
       </Box>
     ),
   ],
@@ -35,5 +58,19 @@ const Template: ComponentStory<typeof Alert> = (args) => <Alert {...args} />;
 export const LunitAlert = Template.bind({});
 LunitAlert.args = {
   severity: "success",
-  title: "This is Title",
+  title: "Danger alert title",
+  children: (
+    <Typography sx={{ fontSize: "14px", fontWeight: 400, lineHeight: "20px" }}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut <u>labore et dolore</u> magna aliqua. A diam
+      sollicitudin tempor id eu nisl nunc mi. Auctor augue mauris augue neque
+      gravida in fermentum.
+    </Typography>
+  ),
+  bottomAction: (
+    <>
+      <Button variant="contained">Primary</Button>
+      <Button variant="text">grey Ghost</Button>
+    </>
+  ),
 };
