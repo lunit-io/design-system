135f22c3b0955c5a94f5d5ac0e2ae0ad153dbcfb
Takhyun Kim <kimkih1218@lunit.io>
2023-01-25 18:42:39 +0900

    docs(button): add argTypes, parameters setting to Base Button story

--- packages/design-system/src/stories/components/Button/BasicButton.stories.tsx
+++ packages/design-system/src/stories/components/Button/BasicButton.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box } from "@mui/material";
+import { action } from "@storybook/addon-actions";
 
 import Button from "@/components/Button/Button";
 
@@ -8,6 +9,50 @@ import type { ComponentStory, ComponentMeta } from "@storybook/react";
 export default {
   title: "Components/Button",
   component: Button,
+  argTypes: {
+    children: {
+      type: "string",
+      defaultValue: "Text",
+    },
+    disabled: {
+      control: {
+        type: "boolean",
+      },
+      defaultValue: false,
+    },
+    size: {
+      control: {
+        type: "select",
+      },
+      options: ["small", "medium", "large"],
+      defaultValue: "small",
+    },
+    onClick: {
+      type: "function",
+      control: {
+        type: "select",
+      },
+      options: ["function", undefined],
+      mapping: {
+        function: action("onClick"),
+        undefined: undefined,
+      },
+      defaultValue: "function",
+      description:
+        "It is a callback function that is called when the button is clicked.",
+    },
+  },
+  parameters: {
+    controls: {
+      include: ["onClick", "children", "disabled", "size"],
+    },
+    docs: {
+      description: {
+        component: `It is a Clickable default Button. For more details, please
+				see [Material-UI Button](https://mui.com/material-ui/react-button/).`,
+      },
+    },
+  },
   decorators: [
     /**
      * TODO
@@ -19,5 +64,5 @@ export default {
 } as ComponentMeta<typeof Button>;
 
 export const BasicButton: ComponentStory<typeof Button> = (arg) => (
-  <Button {...arg}>Text</Button>
+  <Button {...arg}>{arg.children}</Button>
 );
