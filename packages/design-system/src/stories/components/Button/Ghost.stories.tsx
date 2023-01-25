47dcdfb528dc66d430fe66728ed5935b059af753
Takhyun Kim <kimkih1218@lunit.io>
2023-01-25 13:07:18 +0900

    docs(button): add ghost button story

---
+++ packages/design-system/src/stories/components/Button/Ghost.stories.tsx
@@ -0,0 +1,141 @@
+import React from "react";
+import {
+  Box,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+  Typography,
+} from "@mui/material";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Button from "@/components/Button/Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  parameters: {
+    pseudo: {
+      hover: ["#hover"],
+    },
+  },
+  decorators: [
+    /**
+     * TODO
+     * Since Color Token system will be changed, below className should be changed accordingly
+     * Also, the hover color will be set again with the color token system completion
+     */
+    (Story) => <Box className="light1">{Story()}</Box>,
+  ],
+} as ComponentMeta<typeof Button>;
+
+const ButtonTemplate: ComponentStory<typeof Button> = (args) => {
+  return (
+    <>
+      <Table sx={{ width: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell>
+              <Typography variant="body2_m">Ghost Primary</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant="body2_m">Ghost Secondary</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant="body2_m">Ghost Error</Typography>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              <Typography variant="body2_reg">Enable</Typography>
+            </TableCell>
+            <TableCell>
+              <Button kind="ghost">Text</Button>
+            </TableCell>
+            <TableCell>
+              <Button kind="ghost" color="secondary">
+                Text
+              </Button>
+            </TableCell>
+            <TableCell>
+              <Button kind="ghost" color="error">
+                Text
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <Typography variant="body2_reg">Hover</Typography>
+            </TableCell>
+            <TableCell>
+              <Button id="hover" kind="ghost">
+                Text
+              </Button>
+            </TableCell>
+            <TableCell>
+              <Button id="hover" kind="ghost" color="secondary">
+                Text
+              </Button>
+            </TableCell>
+            <TableCell>
+              <Button id="hover" kind="ghost" color="error">
+                Text
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <Typography variant="body2_reg">Focus</Typography>
+            </TableCell>
+            <TableCell>
+              <Button kind="ghost" className="Mui-focusVisible">
+                Text
+              </Button>
+            </TableCell>
+            <TableCell>
+              <Button
+                kind="ghost"
+                color="secondary"
+                className="Mui-focusVisible"
+              >
+                Text
+              </Button>
+            </TableCell>
+            <TableCell>
+              <Button kind="ghost" color="error" className="Mui-focusVisible">
+                Text
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <Typography variant="body2_reg">Disabled</Typography>
+            </TableCell>
+            <TableCell>
+              <Button kind="ghost" disabled>
+                Text
+              </Button>
+            </TableCell>
+            <TableCell>
+              <Button kind="ghost" color="secondary" disabled>
+                Text
+              </Button>
+            </TableCell>
+            <TableCell>
+              <Button kind="ghost" color="error" disabled>
+                Text
+              </Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <br />
+      <br />
+    </>
+  );
+};
+
+export const Ghost = ButtonTemplate.bind({});
