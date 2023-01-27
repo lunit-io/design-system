42b480e7a642ae61fc43c0c584d543f45367bcec
yami03 <seula00027@gmail.com>
2023-01-27 11:10:43 +0900

    feat(toggleButton): add hasIconOnly type

--- packages/design-system/src/components/ToggleButton/ToggleButton.types.ts
+++ packages/design-system/src/components/ToggleButton/ToggleButton.types.ts
@@ -1,27 +1,29 @@
 import type { ToggleButtonProps as MuiToggleButtonProps } from "@mui/material";
 
+interface ToggleButtonBaseProps extends MuiToggleButtonProps {
+  selectedColor?: "primary" | "secondary";
+  // Add type for distinction only because only the style of the icon button is different
+  hasIconOnly?: true;
+}
 interface ContainedToggleButtonProps
-  extends Omit<MuiToggleButtonProps, "variant"> {
+  extends Omit<ToggleButtonBaseProps, "variant"> {
   kind?: "contained";
-  color?: "primary" | "secondary" | "error";
+  color?: "primary" | "secondary";
 }
 
-interface GhostToggleButtonProps extends Omit<MuiToggleButtonProps, "variant"> {
+interface GhostToggleButtonProps
+  extends Omit<ToggleButtonBaseProps, "variant"> {
   kind?: "ghost";
-  color?: "primary" | "secondary" | "error";
+  color?: "primary" | "secondary";
 }
 
 interface OutlinedToggleButtonProps
-  extends Omit<MuiToggleButtonProps, "variant"> {
+  extends Omit<ToggleButtonBaseProps, "variant"> {
   kind?: "outlined";
   color?: "primary";
 }
 
-type ToggleButtonBaseProps =
+export type ToggleButtonProps =
   | ContainedToggleButtonProps
   | GhostToggleButtonProps
   | OutlinedToggleButtonProps;
-
-export type ToggleButtonProps = ToggleButtonBaseProps & {
-  selectedColor: "primary" | "secondary";
-};
