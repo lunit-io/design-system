3a1efc45da44a3f256030fe7347a047123a1e5b1
yami03 <seula00027@gmail.com>
2023-01-27 18:52:10 +0900

    chore(toggleButton): rename toggleButton story

---
+++ packages/design-system/src/stories/components/Button/ToggleButtonColor.stories.tsx
@@ -0,0 +1,172 @@
+import React from "react";
+import {
+  Box,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+  Typography,
+} from "@mui/material";
+import { action } from "@storybook/addon-actions";
+
+import ToggleButton from "@/components/ToggleButton";
+
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
+
+export default {
+  title: "Components/Button",
+  component: ToggleButton,
+  argTypes: {
+    value: {
+      control: false,
+      table: { type: { summary: "any" } },
+    },
+    selected: {
+      control: {
+        type: "boolean",
+      },
+      table: {
+        defaultValue: { summary: "false" },
+      },
+    },
+    selectedColor: {
+      control: false,
+      options: ["primary", "secondary"],
+      description:
+        "The color of the button when it is in an select state. It has nothing to do with active button",
+      table: {
+        defaultValue: { summary: "primary" },
+      },
+    },
+    color: {
+      control: {
+        type: "radio",
+      },
+      options: ["primary", "secondary"],
+      defaultValue: "primary",
+      description: "Button has three Kinds Contained, Ghost",
+      table: {
+        defaultValue: { summary: "primary" },
+      },
+    },
+    kind: {
+      control: {
+        type: "radio",
+      },
+      options: ["contained", "outlined", "ghost"],
+      defaultValue: "contained",
+      table: {
+        defaultValue: { summary: "contained" },
+      },
+    },
+    children: {
+      type: "string",
+      defaultValue: "Text",
+    },
+    disabled: {
+      control: {
+        type: "boolean",
+      },
+      defaultValue: false,
+      table: {
+        defaultValue: { summary: "false" },
+      },
+    },
+    size: {
+      control: {
+        type: "radio",
+      },
+      options: ["small", "medium", "large"],
+      defaultValue: "small",
+      table: {
+        defaultValue: { summary: "small" },
+      },
+    },
+    onClick: {
+      type: "function",
+      control: {
+        type: "select",
+      },
+      options: ["function", undefined],
+      mapping: {
+        function: action("onClick"),
+        undefined: undefined,
+      },
+      defaultValue: "function",
+      description:
+        "It is a callback function that is called when the button is clicked.",
+    },
+  },
+  args: {
+    selected: true,
+    kind: "contained",
+  },
+  parameters: {
+    controls: {
+      include: [
+        "value",
+        "kind",
+        "selectedColor",
+        "onClick",
+        "children",
+        "disabled",
+        "size",
+        "selected",
+        "color",
+      ],
+    },
+    docs: {
+      description: {
+        component: `It is a Clickable default Button. For more details, please
+				see [Material-UI Button](https://mui.com/material-ui/react-button/).`,
+      },
+    },
+  },
+  decorators: [
+    /**
+     * TODO
+     * Since Color Token system will be changed, below className should be changed accordingly
+     * Also, the hover color will be set again with the color token system completion
+     */
+    (Story) => <Box className="light1">{Story()}</Box>,
+  ],
+} as ComponentMeta<typeof ToggleButton>;
+
+const Template: ComponentStory<typeof ToggleButton> = (arg) => (
+  <Table sx={{ width: 330 }}>
+    <TableHead>
+      <TableRow>
+        <TableCell>
+          <Typography variant="body2_m">Selected color</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography variant="body2_m">Primary</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography variant="body2_m">Secondary</Typography>
+        </TableCell>
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      <TableRow>
+        <TableCell>
+          <Typography variant="body2_m">Checked</Typography>
+        </TableCell>
+        <TableCell>
+          <ToggleButton {...arg} value="first">
+            {arg.children}
+          </ToggleButton>
+        </TableCell>
+        <TableCell>
+          <ToggleButton {...arg} selectedColor="secondary" value="second">
+            {arg.children}
+          </ToggleButton>
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+);
+
+export const ToggleButtonStory = Template.bind({});
+ToggleButtonStory.storyName = "ToggleButton: Color";
