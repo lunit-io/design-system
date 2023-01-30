9dd4c7924951447760d185c7cf66bf65544df019
양혜진 <hjyang@lunit.io>
2023-01-30 15:45:30 +0900

    refactor(chip): refactor redundant color token code

--- packages/design-system/src/components/Chip/consts.ts
+++ packages/design-system/src/components/Chip/consts.ts
@@ -5,26 +5,3 @@ export const CHIP_COLORS = {
   WARNING: "warning",
   SUCCESS: "success",
 } as const;
-
-export const COMMON_STYLES = {
-  "&.MuiChip-root": {
-    height: "22px",
-    width: "auto",
-    minWidth: "22px",
-  },
-  "&.Mui-disabled": {
-    opacity: 1,
-  },
-  "& .MuiChip-label": {
-    height: "16px",
-    fontStyle: "normal",
-    fontWeight: 500,
-    fontSize: "12px",
-    lineHeight: "16px",
-    display: "flex",
-    alignItems: "center",
-    textAlign: "center",
-    padding: 0,
-    marginInline: "8px",
-  },
-};
