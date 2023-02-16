eb8a1c376e45e5e25203af6a070178d8e1212a0a
SangYeob Yu <syyu@lunit.io>
2023-02-16 11:47:25 +0900

    fix(design-system): update storybook setting for nx

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -1,6 +1,6 @@
-const path = require("path");
-const alias = require("../config/alias");
-const { getBranchName, getChromaticBranchName } = require("./branch");
+const path = require('path');
+const alias = require('../config/alias');
+const { getBranchName, getChromaticBranchName } = require('./branch');
 const toPath = (filePath) => path.join(process.cwd(), filePath);
 
 module.exports = {
@@ -9,20 +9,20 @@ module.exports = {
     emotionAlias: false,
   },
   stories: [
-    "./Welcome/Welcome.stories.tsx",
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    './Welcome/Welcome.stories.tsx',
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "storybook-addon-pseudo-states",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-addon-pseudo-states',
     {
-      name: "@storybook/addon-storysource",
+      name: '@storybook/addon-storysource',
       options: {
         rule: {
-          include: [path.resolve(__dirname, "../src")], // You can specify directories
+          include: [path.resolve(__dirname, '../src')], // You can specify directories
         },
         loaderOptions: {
           prettierConfig: { printWidth: 80, singleQuote: false },
@@ -34,20 +34,20 @@ module.exports = {
     ...config,
     BRANCH_NAME: process.env.BRANCH_NAME ?? getBranchName(),
     CHROMATIC_APP_ID:
-      process.env.CHROMATIC_APP_ID ?? "62d7d1f2efc8a1e364b5e5e8",
+      process.env.CHROMATIC_APP_ID ?? '62d7d1f2efc8a1e364b5e5e8',
     CHROMATIC_BRANCH_NAME:
       process.env.CHROMATIC_BRANCH_NAME ?? getChromaticBranchName(),
   }),
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   core: {
-    builder: "@storybook/builder-webpack5",
+    builder: '@storybook/builder-webpack5',
   },
   webpackFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       ...alias,
-      "@emotion/core": toPath("node_modules/@emotion/react"),
-      "emotion-theming": toPath("node_modules/@emotion/react"),
+      '@emotion/core': toPath('../../node_modules/@emotion/react'),
+      'emotion-theming': toPath('../../node_modules/@emotion/react'),
     };
     return config;
   },
