86be4695d43ce1ab64f5876b5a9528b17719bb19
SangYeob Yu <syyu@lunit.io>
2023-02-22 18:08:32 +0900

    chore(design-system): setup build and package

--- packages/design-system/webpack.config.js
+++ packages/design-system/webpack.config.js
@@ -1,4 +1,6 @@
+const glob = require("glob");
 const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 const alias = require("./config/alias");
 
 console.log(path.resolve(__dirname, "src"));
@@ -6,13 +8,34 @@ console.log(path.resolve(__dirname, "src"));
 module.exports = {
   mode: "production",
   entry: "./src/index.ts",
+  entry: glob.sync("./src/components/*/index.ts").reduce(
+    function (obj, el) {
+      const name = el.substring(6 /* ./src/ */, el.lastIndexOf("/"));
+      obj[name] = el;
+      return obj;
+    },
+    { main: "./src/index.ts" }
+  ),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: (pathData) => {
+      return pathData.chunk.name === "main" ? "index.js" : "[name]/index.js";
+    },
+    library: {
+      type: "commonjs-static",
+    },
+    clean: true,
+  },
   devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        loader: "ts-loader",
         exclude: /node_modules/,
+        options: {
+          configFile: "tsconfig.build.json",
+        },
       },
     ],
   },
@@ -20,11 +43,9 @@ module.exports = {
     alias,
     extensions: [".tsx", ".ts", ".js"],
   },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "design-system.js",
-    library: {
-      type: "commonjs-static",
-    },
-  },
+  externals: [
+    nodeExternals({
+      modulesFromFile: true,
+    }),
+  ],
 };
