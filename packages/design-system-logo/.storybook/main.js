007c26f57ba63ed31f75a5296f04e9c206252495
양혜진 <hjyang@lunit.io>
2023-04-19 20:14:37 +0900

    chore: remove @storybook/addon-mdx-gfm from logo

--- packages/design-system-logo/.storybook/main.js
+++ packages/design-system-logo/.storybook/main.js
@@ -1,11 +1,15 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
-  "framework": {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
     name: "@storybook/react-webpack5",
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
