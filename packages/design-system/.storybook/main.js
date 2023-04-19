a51a2d8e8297fb10d538505376024df60e2b9226
양혜진 <hjyang@lunit.io>
2023-04-19 20:38:07 +0900

    chore: remove @storybook/addon-mdx-gfm from DS

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -1,51 +1,60 @@
 const path = require("path");
 const alias = require("../config/alias");
-const {
-  getBranchName,
-  getChromaticBranchName
-} = require("./branch");
-const toPath = filePath => path.join(process.cwd(), filePath);
+const { getBranchName, getChromaticBranchName } = require("./branch");
+const toPath = (filePath) => path.join(process.cwd(), filePath);
 module.exports = {
   features: {
     // https://github.com/mui-org/material-ui/issues/24282#issuecomment-967747802
-    emotionAlias: false
+    emotionAlias: false,
   },
-  stories: ["./Welcome/Welcome.stories.tsx", "../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "storybook-addon-pseudo-states", {
-    name: "@storybook/addon-storysource",
-    options: {
-      rule: {
-        include: [path.resolve(__dirname, "../src")] // You can specify directories
-      },
+  stories: [
+    "./Welcome/Welcome.stories.tsx",
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-addon-pseudo-states",
+    {
+      name: "@storybook/addon-storysource",
+      options: {
+        rule: {
+          include: [path.resolve(__dirname, "../src")], // You can specify directories
+        },
 
-      loaderOptions: {
-        prettierConfig: {
-          printWidth: 80,
-          singleQuote: false
-        }
-      }
-    }
-  }, "@storybook/addon-mdx-gfm"],
-  env: config => ({
+        loaderOptions: {
+          prettierConfig: {
+            printWidth: 80,
+            singleQuote: false,
+          },
+        },
+      },
+    },
+  ],
+  env: (config) => ({
     ...config,
     BRANCH_NAME: process.env.BRANCH_NAME ?? getBranchName(),
-    CHROMATIC_APP_ID: process.env.CHROMATIC_APP_ID ?? "62d7d1f2efc8a1e364b5e5e8",
-    CHROMATIC_BRANCH_NAME: process.env.CHROMATIC_BRANCH_NAME ?? getChromaticBranchName()
+    CHROMATIC_APP_ID:
+      process.env.CHROMATIC_APP_ID ?? "62d7d1f2efc8a1e364b5e5e8",
+    CHROMATIC_BRANCH_NAME:
+      process.env.CHROMATIC_BRANCH_NAME ?? getChromaticBranchName(),
   }),
   framework: {
     name: "@storybook/react-webpack5",
-    options: {}
+    options: {},
   },
-  webpackFinal: async config => {
+  webpackFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       ...alias,
       "@emotion/core": toPath("../../node_modules/@emotion/react"),
-      "emotion-theming": toPath("../../node_modules/@emotion/react")
+      "emotion-theming": toPath("../../node_modules/@emotion/react"),
     };
     return config;
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
