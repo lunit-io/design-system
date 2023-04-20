42286e28d620f8897ad53640fb40899713f0d88f
SangYeob Yu <syyu@lunit.io>
2023-04-20 15:43:54 +0900

    chore: remove @storybook/addon-mdx-gfm from icons

--- packages/design-system-icons/.storybook/main.js
+++ packages/design-system-icons/.storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
     emotionAlias: false
   },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-mdx-gfm"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: {
     name: "@storybook/react-webpack5",
     options: {}
