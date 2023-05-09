f19267572553721cd6421cf0bdef7efd23478d72
SangYeob Yu <syyu@lunit.io>
2023-05-10 08:57:37 +0900

    docs(colors): migrate mui example to storybook7

--- packages/design-system/src/stories/foundation/colors/Mui.stories.tsx
+++ packages/design-system/src/stories/foundation/colors/Mui.stories.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { StoryFn, Meta } from "@storybook/react";
 import Pagination from "@mui/material/Pagination";
 
 export default {
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   title: "Foundation/Colors/Mui",
   component: Pagination,
-} as ComponentMeta<typeof Pagination>;
+} as Meta<typeof Pagination>;
 
-export const MuiPagenation: ComponentStory<typeof Pagination> = () => (
+export const MuiPagenation: StoryFn<typeof Pagination> = () => (
   <Pagination count={10} color="primary" />
 );
