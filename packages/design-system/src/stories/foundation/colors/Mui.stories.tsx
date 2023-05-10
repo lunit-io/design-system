cb87590f2496113ce89e6134dded9adba670318f
SangYeob Yu <syyu@lunit.io>
2023-05-10 10:13:21 +0900

    docs(colors): add mui icon example

--- packages/design-system/src/stories/foundation/colors/Mui.stories.tsx
+++ packages/design-system/src/stories/foundation/colors/Mui.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StoryFn, Meta } from "@storybook/react";
 import Pagination from "@mui/material/Pagination";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default {
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
@@ -10,4 +11,8 @@ export default {
 
 export const MuiPagenation: StoryFn<typeof Pagination> = () => (
   <Pagination count={10} color="primary" />
+);
+
+export const MuiIcon: StoryFn<typeof DeleteIcon> = () => (
+  <DeleteIcon color="primary" />
 );
