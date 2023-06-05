89f65fb6f2150917530a492ca40a3af579067b19
SangYeob Yu <syyu@lunit.io>
2023-06-05 12:31:43 +0900

    chore: change yarn to npm

--- apps/nextjs-design-system/pages/index.tsx
+++ apps/nextjs-design-system/pages/index.tsx
@@ -1,11 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { useTheme } from "@mui/material/styles";
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const theme = useTheme();
   return (
     <>
       <Head>
@@ -18,7 +20,9 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
+            <code style={{ color: theme.palette.primary.main }}>
+              pages/index.tsx
+            </code>
           </p>
           <div>
             <a
@@ -26,7 +30,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -110,5 +114,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
