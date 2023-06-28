90173083d5d8e31f4cc53b5e0e53412452e4eb44
SangYeob Yu <syyu@lunit.io>
2023-06-28 09:58:42 +0900

    chore: remove unused storybook setting

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -49,8 +49,6 @@ module.exports = {
     config.resolve.alias = {
       ...config.resolve.alias,
       ...alias,
-      "@emotion/core": toPath("../../node_modules/@emotion/react"),
-      "emotion-theming": toPath("../../node_modules/@emotion/react"),
     };
     return config;
   },
