b71adc7cd9703a4a49089ff887b71a96b861c62c
SangYeob Yu <syyu@lunit.io>
2023-08-18 11:07:45 +0900

    feat(nextjs): apply pretendard as nextjs local font

--- apps/nextjs-test-system/pages/_app.tsx
+++ apps/nextjs-test-system/pages/_app.tsx
@@ -1,9 +1,43 @@
-import { ThemeProvider, CssBaseline, Container } from "@mui/material";
-import { theme } from "@lunit/design-system";
+import {
+  ThemeProvider,
+  CssBaseline,
+  Container,
+  createTheme,
+} from "@mui/material";
+import { deepmerge } from "@mui/utils";
+import { themeOptions } from "@lunit/design-system";
+import localFont from "next/font/local";
+
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
 
+const pretendard = localFont({
+  src: "./assets/PretendardVariable.woff2",
+});
+const theme = createTheme(
+  deepmerge(themeOptions, {
+    typography: {
+      fontFamily: [
+        pretendard.style.fontFamily,
+        "Pretendard",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "system-ui",
+        "Roboto",
+        "Helvetica Neue",
+        "Segoe UI",
+        "Apple SD Gothic Neo",
+        "Noto Sans KR",
+        "Malgun Gothic",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "sans-serif",
+      ].join(","),
+    },
+  })
+);
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
