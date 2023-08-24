e7643aab3f2f557328f7941bb6f764e1851b1389
Jiyeon Seo <jyseo@lunit.io>
2023-08-24 18:26:39 +0900

    feat(color): update color token - Toggle

--- packages/design-system/src/components/Toggle/Toggle.styled.ts
+++ packages/design-system/src/components/Toggle/Toggle.styled.ts
@@ -1,6 +1,6 @@
 import { styled, Switch as MuiSwitch, SwitchProps } from "@mui/material";
 interface ToggleProps extends SwitchProps {
-  toggleSize: 'medium' | 'large'
+  toggleSize: "medium" | "large";
 }
 
 const toggleStyles = {
@@ -19,15 +19,15 @@ const toggleStyles = {
       transition: "all 100ms ease-in-out",
     },
     switchChecked: {
-      transform: 'translateX(10px)',
+      transform: "translateX(10px)",
     },
     thumb: {
-        width: 14,
-        height: 14,
-    }
+      width: 14,
+      height: 14,
+    },
   },
   large: {
-    root:{
+    root: {
       width: 44,
       height: 24,
       borderRadius: 12,
@@ -41,20 +41,20 @@ const toggleStyles = {
       transition: "all 150ms ease-in-out",
     },
     switchChecked: {
-      transform: 'translateX(20px)',
+      transform: "translateX(20px)",
     },
     thumb: {
       width: 20,
       height: 20,
     },
   },
-}
+};
 
 const indeterminateStyles = {
   large: {
     switchChecked: {
-        top: 8,
-        left: -8,
+      top: 8,
+      left: -8,
     },
     track: {
       borderRadius: 12,
@@ -67,8 +67,8 @@ const indeterminateStyles = {
   },
   medium: {
     switchChecked: {
-        top: 6,
-        left: -4,
+      top: 6,
+      left: -4,
     },
     track: {
       borderRadius: 12,
@@ -78,28 +78,29 @@ const indeterminateStyles = {
       width: 12,
       borderRadius: 2,
     },
-  }
-}
+  },
+};
 
 export const CommonToggle = styled(MuiSwitch, {
-  shouldForwardProp: (props) => props !== 'toggleSize',
+  shouldForwardProp: (props) => props !== "toggleSize",
 })<ToggleProps>(({ theme, toggleSize, disabled }) => {
-  const checkedColor = theme.palette.token.component.selectcontrol_on;
-  const uncheckedColor = theme.palette.token.component.selectcontrol_off;
-  const handlerColor = theme.palette.token.component.selectcontrol_handler;
+  const checkedColor = theme.palette.lunit_token.component.selectcontrol_on;
+  const uncheckedColor = theme.palette.lunit_token.component.selectcontrol_off;
+  const handlerColor =
+    theme.palette.lunit_token.component.selectcontrol_handler;
 
   const toggleStyle = toggleStyles[toggleSize];
   const toggleOpacity = disabled ? 0.38 : 1;
 
   return {
     ...toggleStyle.root,
-    display: 'flex',
+    display: "flex",
     padding: 0,
     overflow: "visible",
     backgroundColor: "transparent",
     opacity: toggleOpacity,
     "& .Mui-focusVisible": {
-       // clear default focus style
+      // clear default focus style
       boxShadow: "none",
       background: "transparent",
       borderColor: "transparent",
@@ -112,51 +113,53 @@ export const CommonToggle = styled(MuiSwitch, {
         boxSizing: "border-box",
         top: -3, // border 1px + offset 2px
         left: -3, // border 1px + offset 2px
-      }
+      },
     },
-    '& .MuiSwitch-track': {
+    "& .MuiSwitch-track": {
       opacity: 1,
       borderRadius: 12,
       backgroundColor: uncheckedColor,
     },
-    '& .MuiSwitch-thumb': {
+    "& .MuiSwitch-thumb": {
       ...toggleStyle.thumb,
       boxShadow: "0px 0px 1px rgba(0, 0, 0, 0.4)",
     },
-    '& .MuiSwitch-switchBase': {
+    "& .MuiSwitch-switchBase": {
       ...toggleStyle.switch,
       padding: 2,
       opacity: 1,
-      '&.Mui-checked': {
+      "&.Mui-checked": {
         ...toggleStyle.switchChecked,
         color: handlerColor,
-        '& + .MuiSwitch-track': {
+        "& + .MuiSwitch-track": {
           opacity: 1,
           backgroundColor: checkedColor,
         },
       },
     },
-    '& .Mui-disabled + .MuiSwitch-track': {
+    "& .Mui-disabled + .MuiSwitch-track": {
       opacity: 1,
     },
-  }});
+  };
+});
 
 export const CommonIndeterminateToggle = styled(CommonToggle, {
-  shouldForwardProp: (props) => props !== 'toggleSize',
+  shouldForwardProp: (props) => props !== "toggleSize",
 })(({ toggleSize }) => {
   const indeterminateStyle = indeterminateStyles[toggleSize];
 
   return {
-  padding: 0,
-  cursor: "pointer",
-  '& .MuiSwitch-switchBase.Mui-checked': {
+    padding: 0,
+    cursor: "pointer",
+    "& .MuiSwitch-switchBase.Mui-checked": {
       ...indeterminateStyle.switchChecked,
-      '& + .MuiSwitch-track': {
+      "& + .MuiSwitch-track": {
         ...indeterminateStyle.track,
+      },
     },
-  },
-  '& .MuiSwitch-thumb': {
-    ...indeterminateStyle.thumb,
-    boxShadow: "none",
-  },
-}});
+    "& .MuiSwitch-thumb": {
+      ...indeterminateStyle.thumb,
+      boxShadow: "none",
+    },
+  };
+});
