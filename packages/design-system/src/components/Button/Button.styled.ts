327fb3de37f44c8ea11db444efbd1bdeb641960f
LTakhyunKim <kimkih1218@lunit.io>
2023-10-11 15:54:48 +0900

    fix(button): apply default button generic value to Button style

--- packages/design-system/src/components/Button/Button.styled.ts
+++ packages/design-system/src/components/Button/Button.styled.ts
@@ -11,11 +11,11 @@ import type { ButtonProps } from "./Button.types";
 import type { ToggleButtonProps } from "../ToggleButton/ToggleButton.types";
 import type { Typography } from "@mui/material/styles/createTypography";
 
-type KindStyleParams = Pick<ButtonProps, "kind" | "color"> & {
+type KindStyleParams = Pick<ButtonProps<"button">, "kind" | "color"> & {
   lunit_token: ColorToken;
 };
 
-type CustomButtonProps = ButtonProps & { hasIconOnly: boolean };
+type CustomButtonProps = ButtonProps<"button"> & { hasIconOnly: boolean };
 
 type sizeStyleParams = Pick<
   CustomButtonProps,
