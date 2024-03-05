d5335c5ae5859aafee5a1bc918b335c1110fb3e4
seunghyunOh <sh5h@lunit.io>
2024-03-05 14:22:43 +0900

    feat: replace css with svg

--- packages/design-system/src/components/Radio/Radio.tsx
+++ packages/design-system/src/components/Radio/Radio.tsx
@@ -18,17 +18,43 @@ const CustomRadio = styled(MuiRadio)(({ theme }) => ({
   },
 }));
 
-const DefaultIcon = styled("span")(({ theme }) => ({
-  width: "18px",
-  height: "18px",
-  borderRadius: "50%",
-  border: `1.5px solid ${theme.palette.lunit_token.component.selectcontrol_off}`,
-  margin: "1px",
-}));
+const DefaultIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M10 17.5C14.1421 17.5 17.5 14.1421 17.5 10C17.5 5.85786 14.1421 2.5 10 2.5C5.85786 2.5 2.5 5.85786 2.5 10C2.5 14.1421 5.85786 17.5 10 17.5ZM10 19C14.9706 19 19 14.9706 19 10C19 5.02944 14.9706 1 10 1C5.02944 1 1 5.02944 1 10C1 14.9706 5.02944 19 10 19Z"
+        fill="#99999B"
+      />
+    </svg>
+  );
+};
 
-const CheckedIcon = styled(DefaultIcon)(({ theme }) => ({
-  border: `4px solid ${theme.palette.lunit_token.component.selectcontrol_on}`,
-}));
+const CheckedIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M10 15C12.7614 15 15 12.7614 15 10C15 7.23858 12.7614 5 10 5C7.23858 5 5 7.23858 5 10C5 12.7614 7.23858 15 10 15ZM10 19C14.9706 19 19 14.9706 19 10C19 5.02944 14.9706 1 10 1C5.02944 1 1 5.02944 1 10C1 14.9706 5.02944 19 10 19Z"
+        fill="#00C9EA"
+      />
+    </svg>
+  );
+};
 
 const Radio = (props: RadioProps) => {
   return (
