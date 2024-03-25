603cb70122fce7e6193be970016494c7dcc8de80
Jiyeon Seo <jyseo@lunit.io>
2024-03-25 17:13:07 +0900

    docs(color): update contents

--- packages/design-system/src/stories/foundation/colors/ColorSystem.tsx
+++ packages/design-system/src/stories/foundation/colors/ColorSystem.tsx
@@ -1,72 +1,82 @@
 import React from "react";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, styled } from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+const ColorBox = styled(Box)(({ theme }) => ({
+  textAlign: "center",
+  padding: "4px 0",
+  fontSize: "14px !important",
+  marginTop: "5px !important",
+}));
+
+const BlockBox = styled(Box)(({ theme }) => ({
+  backgroundColor: "#d7d3d3",
+  textAlign: "center",
+  display: "flex",
+  flexDirection: "column",
+  flex: "auto",
+  padding: "10px",
+}));
 
 const ColorSystem = () => {
   return (
     <Box pb={10}>
       <Grid container spacing={2}>
-        <Grid item xs={4}>
-          <Box
-            sx={{
-              backgroundColor: "#d7d3d3",
-              textAlign: "center",
-              display: "flex",
-              flexDirection: "column",
-              padding: "10px",
-            }}
-          >
+        <Grid
+          item
+          xs={4}
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <BlockBox>
             <Box>색상 코드 값</Box>
-            <Box
-              sx={{
-                backgroundColor: "#FFFFFF",
-                textAlign: "center",
-              }}
-            >
-              #FFFFFF
-            </Box>
-          </Box>
-        </Grid>
-        <Grid item xs={4}>
+            <ColorBox bgcolor="#FFFFFF">#FFFFFF</ColorBox>
+            <ColorBox bgcolor="#00C9EA">#00C9EA</ColorBox>
+            <ColorBox bgcolor="#FFB3B8">#FFB3B8</ColorBox>
+          </BlockBox>
           <Box
             sx={{
-              backgroundColor: "#d7d3d3",
-              textAlign: "center",
+              width: "20px",
               display: "flex",
-              flexDirection: "column",
-              padding: "10px",
+              alignItems: "center",
             }}
           >
+            <ArrowForwardIcon />
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={4}
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <BlockBox>
             <Box>Global Palette</Box>
-            <Box
-              sx={{
-                backgroundColor: "#FFFFFF",
-                textAlign: "center",
-              }}
-            >
-              ex) Red10, Blue40
-            </Box>
+            <ColorBox bgcolor="#FFFFFF">grey_0</ColorBox>
+            <ColorBox bgcolor="#00C9EA">lunit_teal_40</ColorBox>
+            <ColorBox bgcolor="#FFB3B8">red_20</ColorBox>
+          </BlockBox>
+          <Box
+            sx={{
+              width: "20px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <ArrowForwardIcon />
           </Box>
         </Grid>
         <Grid item xs={4}>
-          <Box
-            sx={{
-              backgroundColor: "#d7d3d3",
-              textAlign: "center",
-              display: "flex",
-              flexDirection: "column",
-              padding: "10px",
-            }}
-          >
+          <BlockBox>
             <Box>Color Token</Box>
-            <Box
-              sx={{
-                backgroundColor: "#FFFFFF",
-                textAlign: "center",
-              }}
-            >
-              ex) bg_01, btn_text_primary
-            </Box>
-          </Box>
+            <ColorBox bgcolor="#FFFFFF">bg_01</ColorBox>
+            <ColorBox bgcolor="#00C9EA">btn_primary_border</ColorBox>
+            <ColorBox bgcolor="#FFB3B8">chip_error_bg</ColorBox>
+          </BlockBox>
         </Grid>
       </Grid>
     </Box>
