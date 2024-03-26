6b9f22416f646cb1b22c7dd89835b246a82bf65f
SangYeob Yu <syyu@lunit.io>
2024-03-26 13:56:46 +0900

    docs: remove old welcome page
    docs: add pretendard link

--- packages/design-system/.storybook/Welcome/Welcome.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Welcome = () => {
-  return (
-    <div>
-      <h2>Lunit Design System</h2>
-    </div>
-  );
-};
--- packages/design-system/.storybook/manager.js
+++ packages/design-system/.storybook/manager.js
@@ -1,6 +1,14 @@
-import { addons } from '@storybook/addons'
-import yourTheme from './YourTheme';
+import { addons } from "@storybook/addons";
+import { create } from "@storybook/theming";
+import Logo from "./assets/logo.svg";
+
+const theme = create({
+  base: "light",
+  brandTitle: "Lunit Design System Library",
+  brandUrl: "https://www.lunit.io",
+  brandImage: Logo,
+});
 
 addons.setConfig({
-  theme: yourTheme,
+  theme,
 });
