1fb1676a855e00a9aa07b36eb941a0b47573ed89
양혜진 <hjyang@lunit.io>
2024-04-15 14:16:15 +0900

    feat: export Dialog types

--- packages/design-system/src/components/Dialog/index.ts
+++ packages/design-system/src/components/Dialog/index.ts
@@ -1,3 +1,12 @@
 export { default as Dialog } from "./Dialog";
 export { default as DialogTitle } from "./components/DialogTitle";
 export { default as DialogContent } from "./components/DialogContent";
+
+export type {
+  DialogPropsBase,
+  PassiveModalProps,
+  ActionModalProps,
+  ModalProps,
+  NonModalProps,
+  DialogProps,
+} from "./Dialog";
